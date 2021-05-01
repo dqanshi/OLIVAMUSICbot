4c82bc5f879d84c90787c262e51d54ae440c592d
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-05-01 17:36:45 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('馃攷 Finding the song...')
+    m = message.reply('Finding the song...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,7 +41,7 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "鉂� Found Nothing.\n\nTry another keywork or maybe spell it properly."
+            "Found Nothing.\n\nTry another keywork or maybe spell it properly."
         )
         print(str(e))
         return
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**馃幍 Uploaded by @hackelitebotlist**'
+        rep = '**Uploaded by @hackelitebotlist**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('鉂� Error')
+        m.edit('Error')
         print(e)
 
     try:
