7af11b51bb781f5647c9462dffaab9c48f5dc3e8
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-10-17 10:46:34 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -2,12 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
+from config import BOT_USERNAME,CHANNEL_ID,GROUP_ID,  OWNER_ID
 from helpers.filters import other_filters2
 
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
+    await message.reply_photo("https://telegra.ph/file/3f661bff9f20cb886d02c.jpg")
     await message.reply_text(
         f"""**Hey, I'm {bn}
 
@@ -19,17 +20,17 @@ Add me to your group and play music freely!**
             [
                 [
                     InlineKeyboardButton(
-                        "Source Code ", url="https://t.me/mayank_ka_b_for_bot")
+                        "OWNER ", url="https://t.me/(OWNER_ID)")
                   ],[
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/hackelite02"
+                        "Group", url="https://t.me/(GROUP_ID)"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/hackelitebotlist"
+                        "Channel", url="https://t.me/(CHANNEL_ID)"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "Add To Your Group", url="https://t.me/TG_Group_Music_VC_V2_BOT?startgroup=true"
+                        "Add To Your Group", url="https://t.me/BOT_USERNAME?startgroup=true"
                     )]
             ]
         ),
@@ -43,7 +44,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/hackelitebotlist")
+                        "Channel", url="https://t.me/(CHANNEL_ID)")
                 ]
             ]
         )
