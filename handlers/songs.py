56a2a70611f879f6f197b57a803162251d3dc8d7
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-10-17 13:48:47 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -2,7 +2,7 @@ import os
 import requests
 import aiohttp
 import youtube_dl
-
+from config import CHANNEL_ID
 from pyrogram import filters, Client
 from youtube_search import YoutubeSearch
 
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Downloading the song by @hackelitebotlist...")
+    m.edit("Downloading the song by @(CHANNEL_ID)...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Uploaded by @hackelitebotlist**'
+        rep = '**Uploaded by @(CHANNEL_ID)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
